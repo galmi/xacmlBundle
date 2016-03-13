5435e350699943d0a1f91ca73d93ce53d7b50f07
galmi <galmi.ru@gmail.com>
2016-03-14 01:27:06 +0700

    added normalizers for expressions

--- Serializer/Normalizer/PolicyNormalizer.php
+++ Serializer/Normalizer/PolicyNormalizer.php
@@ -17,17 +17,6 @@ use Wingu\OctopusCore\Reflection\ReflectionClassUse;
 class PolicyNormalizer extends AbstractNormalizer
 {
 
-    private $objectManager;
-    private $propertyAccessor;
-
-    public function __construct(ClassMetadataFactoryInterface $classMetadataFactory = null, NameConverterInterface $nameConverter = null, PropertyAccessor $propertyAccessor, ObjectManager $objectManager)
-    {
-        parent::__construct($classMetadataFactory, $nameConverter);
-
-        $this->objectManager = $objectManager;
-        $this->propertyAccessor = $propertyAccessor;
-    }
-
     /**
      * Normalizes an object into a set of arrays/scalars.
      *
@@ -78,89 +67,7 @@ class PolicyNormalizer extends AbstractNormalizer
      */
     public function denormalize($data, $class, $format = null, array $context = array())
     {
-        $allowedAttributes = $this->getAllowedAttributes($class, $context, true);
-        $normalizedData = $this->prepareForDenormalization($data);
-
-        $reflectionClass = new \ReflectionClass($class);
-        $object = $this->instantiateObject($normalizedData, $class, $context, $reflectionClass, $allowedAttributes);
-        if ($this->objectManager->getMetadataFactory()->hasMetadataFor($class)) {
-            $metadata = $this->objectManager->getClassMetadata($class);
-        } else {
-            $metadata = null;
-        }
-
-        foreach ($normalizedData as $attribute => $value) {
-            if ($this->nameConverter) {
-                $attribute = $this->nameConverter->denormalize($attribute);
-            }
-
-            $allowed = $allowedAttributes === false || in_array($attribute, $allowedAttributes);
-            $ignored = in_array($attribute, $this->ignoredAttributes);
-
-            if ($allowed && !$ignored) {
-                try {
-                    $hasAssociation = false;
-                    if ($metadata) {
-                        $hasAssociation = $metadata->hasAssociation($attribute);
-                    }
-                    if ($hasAssociation) {
-                        $associationMapping = $metadata->getAssociationMapping($attribute);
-                        $targetClass = $metadata->getAssociationTargetClass($attribute);
-                        if ($associationMapping['type'] & ClassMetadataInfo::TO_ONE && is_scalar($value)) {
-                            $id = $value;
-                            $value = $this->objectManager->getRepository($targetClass)->find($id);
-                        } else if ($associationMapping['type'] & ClassMetadataInfo::TO_MANY && is_array($value)) {
-                            $data = $value;
-                            $value = [];
-                            foreach ($data as $id) {
-                                $targetObject = $this->objectManager->getRepository($targetClass)->find($id);
-                                if ($targetObject instanceof $targetClass) {
-                                    $value[] = $targetObject;
-                                }
-                            }
-                        }
-                    } else {
-                        $setter = 'set' . $this->camelize($attribute);
-                        if ($reflectionClass->hasMethod($setter)) {
-                            $reflectionMethod = $parameters = $reflectionClass->getMethod($setter);
-                            $reflectionComment  = $reflectionMethod->getDocComment();
-                            $outputArray = [];
-                            $regExp = "/.*\s+(\S+)\[\]\s+\\$".$attribute."/";
-                            preg_match($regExp, $reflectionComment, $outputArray);
-                            if (count($outputArray) == 2 && is_array($value)) {
-                                $valueNew = [];
-                                foreach ($value as $valueOne) {
-                                    if (!class_exists($outputArray[1])) {
-                                        $outputArray[1] = $reflectionClass->getNamespaceName() . '\\' . $outputArray[1];
-                                    }
-                                    $valueNew[] = $this->denormalize($valueOne, $outputArray[1], $format, $context);
-                                }
-                                $value = $valueNew;
-                            } else {
-                                $parameters = $reflectionMethod->getParameters();
-                                if (count($parameters) === 1) {
-                                    $reflectionClassInner = $classInner = $parameters[0]->getClass();
-                                    if ($reflectionClassInner) {
-                                        $classInner = $reflectionClassInner->getName();
-                                        if ($classInner == 'DateTime') {
-                                            $value = new \DateTime($value);
-                                        } elseif (class_exists($classInner)) {
-                                            $value = $this->denormalize($value, $classInner, $format, $context);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    $this->propertyAccessor->setValue($object, $attribute, $value);
-                } catch (NoSuchPropertyException $exception) {
-                    // Properties not found are ignored
-                }
-            }
-        }
-
-        return $object;
-
+        return null;
     }
 
     /**
@@ -174,18 +81,7 @@ class PolicyNormalizer extends AbstractNormalizer
      */
     public function supportsDenormalization($data, $type, $format = null)
     {
-        return $format == 'json' && $type == 'Galmi\XacmlBundle\Entity\Policy';
+        return false;
     }
 
-    /**
-     * Camelizes a given string.
-     *
-     * @param string $string Some string
-     *
-     * @return string The camelized version of the string
-     */
-    private function camelize($string)
-    {
-        return strtr(ucwords(strtr($string, array('_' => ' '))), array(' ' => ''));
-    }
 }
